2d77f1e5fbce206443a80c6de58bdd9dd25aa0fb
pthushanth <pthushanth97@gmail.com>
2023-06-18 01:30:44 +0200

    Add config to all microservices

--- backend/services/gateway/src/app.module.ts
+++ backend/services/gateway/src/app.module.ts
@@ -40,6 +40,46 @@ import { BudgetModule } from "./budgets/budget.module";
           port: 3005,
         },
       },
+      {
+        name: "CATEGORY_SERVICE",
+        transport: Transport.TCP,
+        options: {
+          host: "challenge-categories-service",
+          port: 3007,
+        },
+      },
+      {
+        name: "ALERT_SERVICE",
+        transport: Transport.TCP,
+        options: {
+          host: "challenge-alerts-service",
+          port: 3006,
+        },
+      },
+      {
+        name: "DEBT_SERVICE",
+        transport: Transport.TCP,
+        options: {
+          host: "challenge-debts-service",
+          port: 3008,
+        },
+      },
+      {
+        name: "EVENT_SERVICE",
+        transport: Transport.TCP,
+        options: {
+          host: "challenge-events-service",
+          port: 3009,
+        },
+      },
+      {
+        name: "INCOME_SERVICE",
+        transport: Transport.TCP,
+        options: {
+           host : "challenge-incones-service",
+           port:3010,
+        }
+      }
     ]),
   ],
   controllers: [AppController],
