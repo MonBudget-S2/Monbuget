ca02580ce9a2aa262d87ad2f4086a2917d9ad856
pthushanth <pthushanth97@gmail.com>
2023-06-17 18:53:51 +0200

    Add budget service

--- backend/services/budgets/src/app.service.ts
+++ backend/services/budgets/src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Budget } from './budget.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectRepository(Budget)
+    private budgetRepository: Repository<Budget>,
+  ) {}
+
+  async createBudget(data: any): Promise<any> {
+    const newExpense = this.budgetRepository.create(data);
+    await this.budgetRepository.save(newExpense);
+    return { message: 'Budget created successfully' };
   }
 }
