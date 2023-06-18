274009ee8ebac07db970cbbac424f574f62684a2
chaochao-z <chao19991@hotmail.com>
2023-06-19 00:15:05 +0200

    Events Microservice install all packages

--- backend/services/events/src/app.module.ts
+++ backend/services/events/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventBudgetModule } from './event-budget/event-budget.module';
+import { EventParticipateModule } from './event-participate/event-participate.module';
 
 @Module({
-  imports: [],
+  imports: [EventBudgetModule, EventParticipateModule],
   controllers: [AppController],
   providers: [AppService],
 })
