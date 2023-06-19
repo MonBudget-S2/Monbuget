7d62ec3f877060fd2003100aa5f26a95abf6e491
pthushanth <pthushanth97@gmail.com>
2023-06-19 21:00:52 +0200

    update entities in event microservice

--- backend/services/events/src/app.module.ts
+++ backend/services/events/src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventBudgetModule } from './event-budget/event-budget.module';
 import { EventParticipateModule } from './event-participate/event-participate.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from './typeorm.config';
+import { EventBudget } from './event-budget/entities/event-budget.entity';
+import { EventParticipate } from './event-participate/entities/event-participate.entity';
 
 @Module({
-  imports: [EventBudgetModule, EventParticipateModule],
+  imports: [
+    TypeOrmModule.forRoot(config),
+    TypeOrmModule.forFeature([EventBudget, EventParticipate]),
+    EventBudgetModule,
+    EventParticipateModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/services/events/src/event-participate/entities/event-participate.entity.ts
+++ backend/services/events/src/event-participate/entities/event-participate.entity.ts
@@ -1,5 +1,6 @@
-import {Column, PrimaryGeneratedColumn} from "typeorm";
+import {Entity,Column, PrimaryGeneratedColumn} from "typeorm";
 
+@Entity()
 export class EventParticipate {
     @PrimaryGeneratedColumn('uuid')
     id: string;
