d5059e7dd074e1bf1fb1a42fe171bdabd25ab2db
pthushanth <pthushanth97@gmail.com>
2023-07-03 13:10:21 +0200

    Create interceptor error

--- backend/services/gateway/src/main.ts
+++ backend/services/gateway/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { RcpExceptionsInterceptor } from "./rcp.exceptions.interceptor";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.useGlobalInterceptors(new RcpExceptionsInterceptor());
   await app.listen(3000);
 }
 
