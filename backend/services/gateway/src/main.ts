66c401e23d10ff35bbb9e72e128afcb5c7e2ee76
pthushanth <pthushanth97@gmail.com>
2023-07-03 14:34:00 +0200

    Add event

--- backend/services/gateway/src/main.ts
+++ backend/services/gateway/src/main.ts
@@ -5,7 +5,7 @@ import { RcpExceptionsInterceptor } from "./rcp.exceptions.interceptor";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.useGlobalInterceptors(new RcpExceptionsInterceptor());
+  // app.useGlobalInterceptors(new RcpExceptionsInterceptor());
   await app.listen(3000);
 }
 
