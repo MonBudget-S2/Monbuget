f1645ef99a79fbcf46887f3a6045a176c36f6022
pthushanth <pthushanth97@gmail.com>
2023-07-05 13:21:19 +0200

    crud meeting

--- backend/services/meetings/src/app.controller.ts
+++ backend/services/meetings/src/app.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { AppService } from './app.service';
+import { CreateMeetingDto, UpdateMeetingDto } from './meeting.request';
 
-@Controller()
+@Controller('meeting')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern({ service: 'meeting', action: 'create' })
+  createMeeting(createMeetingDto: CreateMeetingDto) {
+    return this.appService.create(createMeetingDto);
+  }
+
+  @MessagePattern({ service: 'meeting', action: 'getAll' })
+  getAllMeetings() {
+    return this.appService.getAll();
+  }
+
+  @MessagePattern({ service: 'meeting', action: 'getById' })
+  getMeetingById(id: string) {
+    return this.appService.getById(id);
+  }
+
+  @MessagePattern({ service: 'meeting', action: 'update' })
+  updateMeeting(
+    @Payload() payload: { id: string; updateMeetingDto: UpdateMeetingDto },
+  ) {
+    const { id, updateMeetingDto } = payload;
+    return this.appService.update(id, updateMeetingDto);
+  }
+
+  @MessagePattern({ service: 'meeting', action: 'delete' })
+  deleteMeeting(id: string) {
+    return this.appService.delete(id);
   }
 }
--- backend/services/meetings/src/app.service.ts
+++ backend/services/meetings/src/app.service.ts
@@ -1,8 +1,54 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Meeting } from './meeting.entity';
+import { Repository } from 'typeorm';
+import { RpcException } from '@nestjs/microservices';
+import { CreateMeetingDto, UpdateMeetingDto } from './meeting.request';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectRepository(Meeting)
+    private meetingRepository: Repository<Meeting>,
+  ) {}
+
+  async create(createMeetingDto: CreateMeetingDto): Promise<any> {
+    console.log('createMeetingDto', createMeetingDto);
+    const newMeeting = this.meetingRepository.create(createMeetingDto);
+    console.log('newMeeting', newMeeting);
+    await this.meetingRepository.save(newMeeting);
+    return { message: 'Meeting created successfully', newMeeting };
+  }
+
+  async getById(id: string): Promise<Meeting | null> {
+    return this.meetingRepository.findOneBy({ id });
+  }
+
+  async getAll(): Promise<Meeting[]> {
+    return this.meetingRepository.find();
+  }
+
+  async update(
+    id: string,
+    updateMeetingDto: UpdateMeetingDto,
+  ): Promise<Meeting | null> {
+    console.log('updateMeetingDto', updateMeetingDto);
+    const meeting = await this.meetingRepository.findOneBy({ id });
+    if (!meeting) {
+      throw new RpcException('Meeting not found');
+    }
+    const updatedMeeting = await this.meetingRepository.save({
+      ...meeting,
+      ...updateMeetingDto,
+    });
+
+    console.log('updatedMeeting', updatedMeeting);
+
+    return updatedMeeting;
+  }
+
+  async delete(id: string): Promise<boolean> {
+    const result = await this.meetingRepository.delete(id);
+    return result.affected > 0;
   }
 }
