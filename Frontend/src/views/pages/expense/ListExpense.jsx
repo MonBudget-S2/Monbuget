1d6c86e06cdd7553059f1907040d0f102c86a02d
pthushanth <pthushanth97@gmail.com>
2023-07-06 23:35:19 +0200

    Add image view

--- Frontend/src/views/pages/expense/ListExpense.jsx
+++ Frontend/src/views/pages/expense/ListExpense.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { CardContent, Grid, IconButton } from '@mui/material';
+import { CardContent, Grid, IconButton, Dialog, DialogContent, DialogTitle } from '@mui/material';
 import MainCard from 'ui-component/cards/MainCard';
 import { gridSpacing } from 'store/constant';
 import DataTable from 'ui-component/table/DataTable';
@@ -9,20 +9,23 @@ import { Box } from '@mui/system';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import DownloadIcon from '@mui/icons-material/Download';
+import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
 import AddExpense from './AddExpense';
-import { useState } from 'react';
 import { format, parseISO } from 'date-fns';
 
 const ListExpense = ({ expenses, isLoading, setAlertMessage, setIsExpenseChanged }) => {
   const [editingExpense, setEditingExpense] = useState(null);
   const [isAddFormOpen, setIsAddFormOpen] = useState(false);
+  const [imageDialogOpen, setImageDialogOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
   const columns = [
     {
       field: 'actions',
       sortable: false,
       flex: 1,
       headerName: 'Actions',
-      width:150,
+      width: 150,
       renderCell: (params) => (
         <Box sx={{ display: 'flex', justifyContent: 'center', textAlign: 'center' }}>
           <IconButton color="info" aria-label="Modifier" onClick={() => onEdit(params.row.id)}>
@@ -32,11 +35,15 @@ const ListExpense = ({ expenses, isLoading, setAlertMessage, setIsExpenseChanged
             <DeleteIcon />
           </IconButton>
           {params.row.receiptImage && (
-              <IconButton color="info" aria-label="Download Facture" onClick={() => onDownload(params.row.receiptImage)}>
-                <DownloadIcon />
-              </IconButton>
+            <IconButton color="info" aria-label="Download Facture" onClick={() => onDownload(params.row.receiptImage)}>
+              <DownloadIcon />
+            </IconButton>
           )}
-
+          {params.row.receiptImage && (
+            <IconButton color="info" aria-label="Show Image" onClick={() => onShowImage(params.row.receiptImage)}>
+              <InsertPhotoIcon />
+            </IconButton>
+          )}
         </Box>
       )
     },
@@ -51,12 +58,12 @@ const ListExpense = ({ expenses, isLoading, setAlertMessage, setIsExpenseChanged
       headerName: 'Date de réception',
       headerAlign: 'center',
       flex: 1,
-      valueFormatter: (params) => format(new Date(params.value), 'dd/MM/yyyy'),
+      valueFormatter: (params) => format(new Date(params.value), 'dd/MM/yyyy')
     },
     { field: 'description', headerName: 'Description', headerAlign: 'center', flex: 1 },
-    { field: 'location', headerName: 'Lieu', headerAlign: 'center', flex: 1, },
-    { field: 'category', headerName: 'Catégorie', headerAlign: 'center', flex: 1,valueGetter: (params) => params.row.category?.name },
-    { field: 'eventBudget', headerName: 'Budget', headerAlign: 'center', flex: 1,valueGetter: (params) => params.row.eventBudget?.name }
+    { field: 'location', headerName: 'Lieu', headerAlign: 'center', flex: 1 },
+    { field: 'category', headerName: 'Catégorie', headerAlign: 'center', flex: 1, valueGetter: (params) => params.row.category?.name },
+    { field: 'eventBudget', headerName: 'Budget', headerAlign: 'center', flex: 1, valueGetter: (params) => params.row.eventBudget?.name }
   ];
 
   const onEdit = (id) => {
@@ -78,17 +85,33 @@ const ListExpense = ({ expenses, isLoading, setAlertMessage, setIsExpenseChanged
     }
   };
 
-  const onDownload = async (id) =>{
+  const onDownload = async (id) => {
     const res = await expenseService.downloadFile(id);
     const url = window.URL.createObjectURL(new Blob([res.data]));
     const link = document.createElement('a');
     link.href = url;
-    link.setAttribute('download', id); // Remplacez 'file.png' par le nom de fichier souhaité
+    link.setAttribute('download', id); // Replace 'file.png' with the desired file name
 
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
-  }
+  };
+
+  const onShowImage = async (image) => {
+    try {
+      const res = await expenseService.downloadFile(image);
+      const url = URL.createObjectURL(new Blob([res.data]));
+      setSelectedImage(url);
+      setImageDialogOpen(true);
+    } catch (error) {
+      console.log('Error retrieving image:', error);
+    }
+  };
+
+  const onCloseImageDialog = () => {
+    setSelectedImage(null);
+    setImageDialogOpen(false);
+  };
 
   return (
     <>
@@ -110,6 +133,11 @@ const ListExpense = ({ expenses, isLoading, setAlertMessage, setIsExpenseChanged
           </Grid>
         </CardContent>
       </MainCard>
+
+      <Dialog open={imageDialogOpen} onClose={onCloseImageDialog} maxWidth="md" fullWidth>
+        <DialogTitle>Receipt Image</DialogTitle>
+        <DialogContent>{selectedImage && <img src={selectedImage} alt="Receipt" style={{ maxWidth: '100%' }} />}</DialogContent>
+      </Dialog>
     </>
   );
 };
