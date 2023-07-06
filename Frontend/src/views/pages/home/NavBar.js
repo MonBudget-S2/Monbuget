5f98e056965971fa91ea0505b7908ed4175d6c96
ronantr <noob93500@gmail.com>
2023-07-06 23:04:42 +0200

    edit navbar

--- Frontend/src/views/pages/home/NavBar.js
+++ Frontend/src/views/pages/home/NavBar.js
@@ -2,13 +2,9 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
-// import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 import Link from '@mui/material/Link';
 import { useSelector } from 'react-redux';
@@ -16,60 +12,14 @@ import { getUser } from 'store/authSlice';
 import BackgroundLetterAvatars from 'ui-component/avatar/BackgroundLetterAvatar';
 import logo from 'assets/logo/logo-white.svg';
 
-// const pages = ['Home', 'Products', 'Pricing', 'About'];
 const HomeNavBar = () => {
   const user = useSelector(getUser);
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
 
   return (
-    <AppBar position="static" sx={{ background: 'black' }}>
+    <AppBar sx={{ background: 'black' }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <img src={logo} alt="MonBudget" width="150" />
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left'
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left'
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' }
-              }}
-            >
-              {/* {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))} */}
-            </Menu>
-          </Box>
           <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
           <Typography
             variant="h5"
@@ -77,7 +27,7 @@ const HomeNavBar = () => {
             component="a"
             href=""
             sx={{
-              mr: 2,
+              mr: 3,
               display: { xs: 'flex', md: 'none' },
               flexGrow: 1,
               fontFamily: 'monospace',
@@ -87,18 +37,12 @@ const HomeNavBar = () => {
               textDecoration: 'none'
             }}
           >
+            {/* Add your text here */}
           </Typography>
-          {/* <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button key={page} onClick={handleCloseNavMenu} sx={{ my: 2, color: 'white', display: 'block' }}>
-                {page}
-              </Button>
-            ))}
-          </Box> */}
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'flex-end',  alignItems: 'center' }}>
+          <Box sx={{ flexGrow: 1, display: {  md: 'flex' }, justifyContent: 'flex-end', alignItems: 'center' }}>
             {user?.id ? (
-              <Box sx={{ display: 'flex', alignItems: 'center' }}> 
-                <Link href="/dashboard" sx={{ textDecoration: 'none', color: 'white', marginLeft: '8px', display: 'flex', alignItems: 'center'   }}>
+              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <Link href="/dashboard" sx={{ textDecoration: 'none', color: 'white', marginLeft: '8px', display: 'flex', alignItems: 'center' }}>
                   {user?.userInfo?.lastname + ' ' + user?.userInfo?.firstname}
                   <BackgroundLetterAvatars fullname={`${user.userInfo?.firstName} ${user.userInfo?.lastName}`} sx={{ marginRight: '8px' }} />
                 </Link>
@@ -106,15 +50,11 @@ const HomeNavBar = () => {
             ) : (
               <>
                 <Link href="/login">
-                  <Button onClick={handleCloseNavMenu} sx={{ my: 2, color: 'white', display: 'block' }}>
-                    Se connecter
-                  </Button>
+                  <Button sx={{ my: 2, color: 'white', display: 'block' }}>Se connecter</Button>
                 </Link>
 
                 <Link href="/register">
-                  <Button onClick={handleCloseNavMenu} sx={{ my: 2, color: 'white', display: 'block' }}>
-                    S&apos;inscrire
-                  </Button>
+                  <Button sx={{ my: 2, color: 'white', display: 'block' }}>S&apos;inscrire</Button>
                 </Link>
               </>
             )}
@@ -126,3 +66,4 @@ const HomeNavBar = () => {
 };
 
 export default HomeNavBar;
+  
--- README.md
+++ README.md
@@ -2,13 +2,14 @@
 
 Challenge Semestriel 2 
 
-Nest.js
+Nest.js 
 
 
 
 ## Requirements
 
 * [Install Docker](https://docs.docker.com/get-docker/) (with [docker-compose](https://docs.docker.com/compose/install/))
+* or Kubernetes 
 
 ## Getting Started
 
@@ -16,29 +17,64 @@ Make a git clone of this repository and in the created folder, run this command 
 
 ```bash
 git https://github.com/MonBudget-S2/Monbuget.git
-cd MonBuget/Nest
-docker-compose build --pull --no-cache
-docker-compose up -d
-cd ..
+cd MonBuget/backend
+make start
+```
+or
+```bash
+git https://github.com/MonBudget-S2/Monbuget.git
+cd MonBuget/backend
+	docker compose up -d
+	docker compose exec challenge-gateway-service rm -rf node_modules
+	docker compose exec challenge-gateway-service npm install 
+	docker compose exec -d challenge-gateway-service npm run start:dev 
+	docker compose exec challenge-users-service rm -rf node_modules
+	docker compose exec challenge-users-service npm install
+	docker compose exec -d challenge-users-service npm run start:dev 
+	docker compose exec challenge-incomes-service  rm -rf node_modules
+	docker compose exec challenge-incomes-service npm install
+	docker compose exec -d challenge-incomes-service npm run start:dev 
+	docker compose exec challenge-expenses-service rm -rf node_modules
+	docker compose exec challenge-expenses-service npm install 
+	docker compose exec -d challenge-expenses-service npm run start:dev 
+	docker compose exec challenge-categories-service rm -rf node_modules
+	docker compose exec challenge-categories-service npm install 
+	docker compose exec -d challenge-categories-service npm run start:dev 
+	docker compose exec challenge-events-service rm -rf node_modules
+	docker compose exec challenge-events-service npm install 
+	docker compose exec -d challenge-events-service npm run start:dev 
+	docker compose exec challenge-debts-service rm -rf node_modules
+	docker compose exec challenge-debts-service npm install 
+	docker compose exec -d challenge-debts-service npm run start:dev 
+	docker compose exec challenge-alerts-service rm -rf node_modules
+	docker compose exec challenge-alerts-service npm install 
+	docker compose exec -d challenge-alerts-service npm run start:dev 
+	docker compose exec challenge-budgets-service  rm -rf node_modules
+	docker compose exec challenge-budgets-service npm install 
+	docker compose exec -d challenge-budgets-service npm run start:dev 
+	docker compose exec challenge-meetings-service  rm -rf node_modules
+	docker compose exec challenge-meetings-service npm install 
+	docker compose exec -d challenge-meetings-service npm run start:dev 
+```
+for start front in dev
+
+```bash
 cd Frontend
-netlify deploy --dir web-build
-docker compose exec php sh -c '
-    set -e
-    apk add openssl
-    php bin/console lexik:jwt:generate-keypair
-    setfacl -R -m u:www-data:rX -m u:"$(whoami)":rwX config/jwt
-    setfacl -dR -m u:www-data:rX -m u:"$(whoami)":rwX config/jwt
-    docker compose exec php php bin/console d:d:d --force
-    docker compose exec php php bin/console d:d:c
-    docker compose exec php php bin/console d:s:u --force
-    docker compose exec php php bin/console d:f:l --no-interaction
-'
+docker-compose up -d --build
 ```
 
 ```
-# URL
-https://localhost
+# URL DEV API
+https://localhost:3000
+# URL DEV FRONT
+https://localhost:3001
 ```
+
+### URL PROD
+
+- https://monbuget.fr
+
+- https://api.monbuget.fr
 
 ## Contributors
 
